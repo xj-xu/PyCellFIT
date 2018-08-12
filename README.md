537a66d2b03e2008bd27d26389e8853a8068c783
xj <xj@xjs-computing-space.local>
2018-08-11 22:56:48 -0500

    added file io notes

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ From [Wayne Brodland](http://www.civil.uwaterloo.ca/brodland/cellFIT.asp)'s web 
 CellFIT is a package of force inference equations and assessment tools that calculates the forces in cell edges based on their geometries. In this approach, cells in an image are segmented and equilibrium equations are constructed for each triple junction based solely on edge tensions and the angles at which edges approach each junction. Solving the resulting system of tension equations yields a set of relative edge tensions whose scaling must be determined from data external to the image.
 
 In this implementation, the goal is to optimize the user interface for improved accessibility of CellFIT to scientists of all backgrounds across all platforms (Linux, MacOS, Windows). 
+Applying this tool to the cell mechanics of wound healing is of particular interest.
+
+[Young-Laplace Equation](https://en.wikipedia.org/wiki/Young%E2%80%93Laplace_equation)
--- driver.py
+++ driver.py
@@ -1,34 +1,51 @@
 # driver.py: main script for Python implementation of
 # CellFIT, which I named PyCellFIT for descriptive effects.
-# 
-# Created 08/07/18 by XJ
-#
-# Edits:
-#
-#
+""" 
+Created 08/07/18 by XJ
+Edits:
 
+"""
+
+# IMPORT PACKAGES
+import numpy as np
+
+
+"""
 # INPUT FINITE ELEMENT OBJECT
+with open('foe_sample.txt') as f:
+	data = f.read()
+print(data)
+nodes = np.zeros()
+data = np.loadtxt('foe_sample.txt',skiprows=7)
+print(data)
+"""
 
+# MESH GENERATION
+
+"""
+Input: Segmented image in raster form
+		 e.g. /ZazuCellFITSoftware/Data/Dragonfly/SegmentedImages
+Ouput: Finite element object with 'BEGIN_FILE'
+		 e.g. ZazuCellFITSoftware/Data/Dragonfly/Meshes/Mesh001
+Main:
+"""
 
 
 # ANGLE DETERMINATION
-
+"""
+Input: tempdate.txt Model.cpp line 81
+Ouput: fileName.c_str()
+Main: Zazu/FileIO.cpp WriteLayerData() line 36
+"""
 
 
 # CURVATURE DETERMINATION
 
+"""
+Input:
+Ouput:
+Main:
+"""
 
 
 # CONSTRUCT YOUNG'S EQUATIONS
-
-
-
-# ASSEMBLE, CONSTRAIN AND SOLVE TENSION EQS
-
-
-
-# CONSTRUCT LAPLACE EQUATIONS
-
-
-
-# ASSEMBLE, CONSTRAIN AND SOLVE PRESSURE EQS
